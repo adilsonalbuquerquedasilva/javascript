13a28b6983efd553de862140ea3281fe528f1c4f
Adilson albuquerque da silva <adilson.2024.official@gmail.com>
2025-11-03 17:20:27 -0300

    exercicios aulas 6 10 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 71
+
+console.log(`A velocidade é ${vel}km`)
+if (vel >70){
+    console.log(`Você ultrapassou a velocidade ${vel}km`)
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'brasil';
+
+if(pais != 'brasil'){
+    console.log('Você é um estrangeiro');
+}else{
+    console.log('Você é brasileiro')
+}
